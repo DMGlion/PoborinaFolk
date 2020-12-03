d8f3954e125974c84aa6ff9bd320df125a66f39b
David Moya <davidmgarcia6@gmail.com>
2020-12-03 23:56:46 +0000

    todo

--- PoborinaFolk.Android/MainActivity.cs
+++ PoborinaFolk.Android/MainActivity.cs
@@ -25,7 +25,8 @@ namespace PoborinaFolk.Droid
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             Xamarin.FormsGoogleMaps.Init(this, savedInstanceState); // initialize for Xamarin.Forms.GoogleMaps
-            CrossMediaManager.Current.Init();
+            //TODO fix issue with croos media manager the app will crash on the device do to the android version, Android version will have ot be increased
+            //CrossMediaManager.Current.Init();
             LoadApplication(new App());
         }
         public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
