d7385e6f29622b0ac701c68ad1ef97ffc2c651a7
David Moya <davidmgarcia6@gmail.com>
2020-12-04 00:00:40 +0000

    refactoring

--- PoborinaFolk/ViewModels/PlayerMainViewModel.cs
+++ PoborinaFolk/ViewModels/PlayerMainViewModel.cs
@@ -7,9 +7,9 @@ using Xamarin.Forms;
 
 namespace PoborinaFolk.ViewModels
 {
-    public class MainViewModel : BaseViewModel
+    public class PlayerMainViewModel : BaseViewModel
     {
-        public MainViewModel()
+        public PlayerMainViewModel()
         {
             musicList = GetMusics();
             recentMusic = musicList.Where(x => x.IsRecent == true).FirstOrDefault();
@@ -66,9 +66,14 @@ namespace PoborinaFolk.ViewModels
         {
             return new ObservableCollection<Music>
             {
-                new Music { Title = "Cancion", Artist = "Nombre Grupo",
+                new Music { Title = "Cancion1", Artist = "Nombre Grupo1",
                     Url = "https://freesound.org/data/previews/425/425556_4929134-lq.mp3",
                     CoverImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTusundbRTmm2yqrnQ2JFagM1HyzafDL0I07w&usqp=CAU",
+                    IsRecent = true},
+
+                new Music { Title = "Cancion2", Artist = "Nombre Grupo2",
+                    Url = "https://freesound.org/data/previews/9/9351_24818-lq.mp3",
+                    CoverImage = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTRVKeubeVQI7XuJQg4-JOvy4uXRZpveSIUMQ&usqp=CAU",
                     IsRecent = true},
             };
         }
