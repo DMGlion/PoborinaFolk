01dd32ac74430ac940ebd065a703751302eb15b7
David Moya <davidmgarcia6@gmail.com>
2020-12-05 02:35:10 +0000

    Added New View for Info View, ObjectModel for Info

---
+++ PoborinaFolk/Model/Info.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+namespace PoborinaFolk.Model
+{
+    public class Info
+    {
+            public string Image { get; set; }
+            public string Information { get; set; }
+            public string InformationAdd { get; set; }
+            public string PhoneNumber { get; set; }
+            public string PhoneImage { get; set; }
+    }
+}
+
---
+++ PoborinaFolk/ViewModels/InfoMainViewModel.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using PoborinaFolk.Model;
+using System.Collections.ObjectModel;
+
+namespace PoborinaFolk.ViewModels
+{
+    public class InfoMainViewModel
+    {
+        public InfoMainViewModel()
+        {
+            infoList = GetInfo();
+        }
+
+        public ObservableCollection<Info> InfoList
+        {
+            get { return infoList; }
+            set
+            {
+                infoList = value;
+            }
+        }
+
+        ObservableCollection<Info> infoList;
+        private ObservableCollection<Info> GetInfo()
+        {
+            return new ObservableCollection<Info>
+            {
+                new Info { Image = "emergency.png", Information = "Emergencias", PhoneNumber = "112", PhoneImage="call.png" },
+                new Info { Image = "ambulance.png", Information = "Ambulancia", PhoneNumber = "061", PhoneImage="call.png" },
+                new Info { Image = "police.png", Information = "Guardia Civil", PhoneNumber = "062", PhoneImage="call.png" },
+                new Info { Image = "fireman.png", Information = "Bomberos", PhoneNumber = "080", PhoneImage="call.png" },
+                new Info { Image = "clinic.png", Information = "Centro de salud cercano,", InformationAdd="Calle Alcalá, Cedrillas"},
+                new Info { Image = "pharmacy.png", Information = "Farmacia mas cercana,", InformationAdd="Calle Mayor, Cedrillas"},
+                new Info { Image = "visa.gif", Information = "Cajero mas cecano,", InformationAdd="Calle Mayor, Cedrillas"},
+                new Info { Image = "informa.png", Information = "información del Festival", InformationAdd="Caseta del Escenario Principal"}
+            };
+        }
+    }
+}
