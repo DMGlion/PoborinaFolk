5943cc3101aa7e444b7576b9519238740e4b87d8
David Moya <davidmgarcia6@gmail.com>
2020-12-06 00:32:17 +0000

    removed all infoPage XAML and refactored infoPage1

--- PoborinaFolk/InfoPage.xaml.cs
+++ PoborinaFolk/InfoPage.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using Xamarin.Essentials;
+
 using Xamarin.Forms;
 
 namespace PoborinaFolk
@@ -12,53 +13,18 @@ namespace PoborinaFolk
             InitializeComponent();
         }
 
-        private void btnCallEmergency(object sender, EventArgs e)
+        private void Avaliar(object sender, EventArgs e)
         {
+            Console.WriteLine(((Button)sender).CommandParameter);
+
             try
             {
-                PhoneDialer.Open("112");
+                PhoneDialer.Open((String)(((Button)sender).CommandParameter));
             }
             catch
             {
-                DisplayAlert("Unable to make the call to 112" , "please enter number", "OK");
+                DisplayAlert("Unable to make the call", "No Sim", "OK");
             }
         }
-
-        private void btnCallAmbulance(object sender, EventArgs e)
-        {
-            try
-            {
-                PhoneDialer.Open("061");
-            }
-            catch
-            {
-                DisplayAlert("Unable to make the call to 061", "please enter number", "OK");
-            }
-        }
-
-        private void btnPolice(object sender, EventArgs e)
-        {
-            try
-            {
-                PhoneDialer.Open("062");
-            }
-            catch
-            {
-                DisplayAlert("Unable to make the call to 062", "please enter number", "OK");
-            }
-        }
-
-        private void btnFireman(object sender, EventArgs e)
-        {
-            try
-            {
-                PhoneDialer.Open("080");
-            }
-            catch
-            {
-                DisplayAlert("Unable to make the call to 080", "please enter number", "OK");
-            }
-        }
-
     }
 }
