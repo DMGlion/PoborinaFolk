f5d7e6f07f3f6dad1191f7b4dcb3d651f9690429
David Moya <davidmgarcia6@gmail.com>
2020-12-07 17:30:27 +0000

    ConcertViewModel data Bind

--- PoborinaFolk/ViewModels/ConcertEventsViewModel.cs
+++ PoborinaFolk/ViewModels/ConcertEventsViewModel.cs
@@ -1,10 +1,34 @@
 ﻿using System;
+using PoborinaFolk.Model;
+using System.Collections.ObjectModel;
+
 namespace PoborinaFolk.ViewModels
 {
     public class ConcertEventsViewModel
     {
         public ConcertEventsViewModel()
         {
+            fridayList = GetFridayList();
+        }
+
+        public ObservableCollection<Concerts> FridayList
+        {
+            get { return fridayList; }
+            set
+            {
+                fridayList = value;
+            }
+        }
+
+        ObservableCollection<Concerts> fridayList;
+        private ObservableCollection<Concerts> GetFridayList()
+        {
+            return new ObservableCollection<Concerts>
+            {
+                new Concerts {ImageList="ViernesGrupo.png", NameBand="first Band Name", TimePlay="22h30", PlacePlay="Escenario Central"},
+                new Concerts {ImageList="ViernesGrupo.png", NameBand="first Band Name", TimePlay="22h30", PlacePlay="Escenario Central"}
+            };
+
         }
     }
 }
